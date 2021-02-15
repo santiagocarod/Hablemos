7eb3195f2d2379de013a8094f839d83e596b4652
santiagocarod <santiagocaroduque@gmail.com>
2021-02-15 18:49:43 -0500

    unused imports

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'start.dart';
-import 'package:firebase_core/firebase_core.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
