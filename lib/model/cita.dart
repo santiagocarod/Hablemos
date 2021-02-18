6e03419e56b575dfb09a1b311f6a0b4ad738b039
Santiago Caro Duque <santiagocaroduque@gmail.com>
2021-02-18 15:59:15 -0500

    Added citas_provider with minimal logic

--- lib/model/cita.dart
+++ lib/model/cita.dart
@@ -8,51 +8,9 @@ class Cita {
   String _tipo;
   bool _estado;
 
-  String get uidPaciente => _uidPaciente;
 
-  set uidPaciente(String value) {
-    _uidPaciente = value;
-  }
+  Cita(this._uidPaciente, this._uidProfesional, this._dateTime, this._costo,
+      this._lugar, this._especialidad, this._tipo){this._estado = false;}
 
-  String get uidProfesional => _uidProfesional;
 
-  bool get estado => _estado;
-
-  set estado(bool value) {
-    _estado = value;
-  }
-
-  String get tipo => _tipo;
-
-  set tipo(String value) {
-    _tipo = value;
-  }
-
-  String get especialidad => _especialidad;
-
-  set especialidad(String value) {
-    _especialidad = value;
-  }
-
-  String get lugar => _lugar;
-
-  set lugar(String value) {
-    _lugar = value;
-  }
-
-  int get costo => _costo;
-
-  set costo(int value) {
-    _costo = value;
-  }
-
-  DateTime get dateTime => _dateTime;
-
-  set dateTime(DateTime value) {
-    _dateTime = value;
-  }
-
-  set uidProfesional(String value) {
-    _uidProfesional = value;
-  }
 }
