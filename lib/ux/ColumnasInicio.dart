83b51d6a9af7a99ece11e614e0af890cd11087a2
n-villate <n-villate@javeriana.edu.co>
2021-02-21 09:49:37 -0500

    Update ColumnasInicio.dart

--- lib/ux/ColumnasInicio.dart
+++ lib/ux/ColumnasInicio.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:hablemos/start.dart';
 import '../constants.dart';
 
 class ColumnasInicio extends StatelessWidget {
