5dd5d07a41d8e824fbbfe62842bd8d2a49f7c211
Santiago Caro Duque <santiagocaroduque@gmail.com>
2021-02-23 17:39:17 -0500

    added route from date Details

--- lib/presentation/pacient/dateDetails.dart
+++ lib/presentation/pacient/dateDetails.dart
@@ -242,7 +242,9 @@ Widget _buttons(BuildContext context) {
               ),
               shadowColor: Colors.black,
             ),
-            onPressed: () {},
+            onPressed: () {
+              Navigator.pushNamed(context, 'AdjuntarPago');
+            },
           ),
         ),
         SizedBox(
