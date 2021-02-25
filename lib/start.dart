28abfdd0946d80a9fceced2edf4b55760669d9e5
Nicolas Lopez <nlopez97f@gmail.com>
2021-02-25 10:46:50 -0500

    cambios boton start

--- lib/start.dart
+++ lib/start.dart
@@ -9,6 +9,7 @@ class StartFireBase extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false, // Avoid the debug message
       home: FutureBuilder(
         future: _firebaseApp,
         builder: (context, snapshot) {
@@ -32,14 +33,11 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    return MaterialApp(
-      debugShowCheckedModeBanner: false, // Avoid the debug message
-      home: Stack(
-        children: <Widget>[
-          _background(context, size),
-          _startButton(context, size),
-        ],
-      ),
+    return Stack(
+      children: <Widget>[
+        _background(context, size),
+        _startButton(context, size),
+      ],
     );
   }
 }
@@ -56,42 +54,55 @@ Widget _background(BuildContext context, Size size) {
 }
 
 Widget _startButton(BuildContext context, Size size) {
-  return SingleChildScrollView(
-    // Start buttom (COMENZAR) =========================================
-    child: Container(
-      width: size.width,
-      padding: EdgeInsets.symmetric(
-          horizontal: 100, vertical: (size.height / 2) + 50),
-      child: Padding(
-        padding: EdgeInsets.only(bottom: 347.0),
-        child: SizedBox(
-          width: 200.0,
-          height: 42.0,
-          child: ElevatedButton(
-            child: Text(
-              "COMENZAR",
-              style: TextStyle(
-                  fontSize: 21.0,
-                  color: Colors.black,
-                  letterSpacing: 3,
-                  fontFamily: 'PoppinSemiBold'),
-            ),
-            style: ElevatedButton.styleFrom(
-              primary: Colors.white,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(32.0),
-              ),
-            ),
-            onPressed: () {
-              /*AuthService authService = new AuthService();
-                authService.signUp("santiago@gmail.com", "123456");
-                print(
-                "User : ${authService.getCurrentUser().then((value) => print(value))}");*/
-              Navigator.pushNamed(context, 'start');
-            },
-          ),
-        ),
+  return Column(
+    crossAxisAlignment: CrossAxisAlignment.center,
+    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    children: [
+      SizedBox(
+        width: double.infinity,
       ),
-    ),
+      SizedBox(
+        width: double.infinity,
+      ),
+      SizedBox(
+        width: double.infinity,
+      ),
+      Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          SizedBox(width: 50.0),
+          SizedBox(
+            height: 42.0,
+            child: ElevatedButton(
+              child: Text(
+                "COMENZAR",
+                style: TextStyle(
+                    fontSize: 21.0,
+                    color: Colors.black,
+                    letterSpacing: 3,
+                    fontFamily: 'PoppinSemiBold'),
+              ),
+              style: ElevatedButton.styleFrom(
+                primary: Colors.white,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(32.0),
+                ),
+              ),
+              onPressed: () {
+                /*AuthService authService = new AuthService();
+              authService.signUp("santiago@gmail.com", "123456");
+              print(
+              "User : ${authService.getCurrentUser().then((value) => print(value))}");*/
+                Navigator.pushNamed(context, 'start');
+              },
+            ),
+          ),
+          SizedBox(width: 50.0),
+        ],
+      ),
+      SizedBox(
+        width: double.infinity,
+      )
+    ],
   );
 }
