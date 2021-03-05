16c682831beb8fc8318520795546a0db266191fb
Santiago Caro Duque <santiagocaroduque@gmail.com>
2021-03-05 13:15:52 -0500

    fixed formatting

--- lib/presentation/pantallaInicio.dart
+++ lib/presentation/pantallaInicio.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:hablemos/model/paciente.dart';
-import 'package:hablemos/services/providers/pacientes_provider.dart';
+/*import 'package:hablemos/model/paciente.dart';
+import 'package:hablemos/services/providers/pacientes_provider.dart';*/
 import '../constants.dart';
 import 'body.dart';
 import 'package:hablemos/services/auth.dart';
@@ -26,14 +26,13 @@ class _PantallaInicioState extends State<PantallaInicio> {
 
   @override
   Widget build(BuildContext context) {
-    Paciente paciente = PacientesProvider.getPaciente();
+    //Paciente paciente = PacientesProvider.getPaciente();
     Size size = MediaQuery.of(context).size;
     return Scaffold(
         appBar: buildAppBar(size),
         backgroundColor: Colors.transparent,
         body: Body(
           size: size,
-
           username: username,
         ));
   }
