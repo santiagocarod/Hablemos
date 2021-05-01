7950811709972e4492aef5867a1c65419b80ea98
n-villate <n-villate@javeriana.edu.co>
2021-05-01 16:43:56 -0500

    import

--- lib/presentation/professional/appointments/editarCita.dart
+++ lib/presentation/professional/appointments/editarCita.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:hablemos/constants.dart';
 import 'package:hablemos/model/cita.dart';
-import 'package:hablemos/ux/atoms.dart';
 import 'package:intl/intl.dart';
 
 class EditCitaPro extends StatelessWidget {
