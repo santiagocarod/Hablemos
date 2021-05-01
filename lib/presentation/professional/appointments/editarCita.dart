e6b1865f0fd7d24cbf86a7e4ada4d91b79137027
n-villate <n-villate@javeriana.edu.co>
2021-05-01 14:00:18 -0500

    Pagina Faltante de Editar Cita

--- lib/presentation/professional/appointments/editarCita.dart
+++ lib/presentation/professional/appointments/editarCita.dart
@@ -5,7 +5,7 @@ import 'package:hablemos/model/cita.dart';
 import 'package:hablemos/ux/atoms.dart';
 import 'package:intl/intl.dart';
 
-class DetalleCitaPro extends StatelessWidget {
+class EditCitaPro extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Cita cita = ModalRoute.of(context).settings.arguments;
@@ -44,48 +44,59 @@ class DetalleCitaPro extends StatelessWidget {
               children: <Widget>[
                 Container(
                   padding: EdgeInsets.only(
-                    top: size.height * 0.025,
+                    top: size.height * 0.017,
                   ),
                   child: Column(
                     children: <Widget>[
-                      _pageHeader(context, size, "Detalle Cita"),
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: <Widget>[
-                          Container(
-                            alignment: Alignment.center,
-                            //margin: EdgeInsets.all(20),
-                            width: 359.0,
-                            height: 599.0,
-                            decoration: BoxDecoration(
-                              color: kBlanco,
-                              boxShadow: [
-                                BoxShadow(
-                                    offset: Offset(0, 0),
-                                    blurRadius: 5.0,
-                                    color: Colors.grey.withOpacity(0.5)),
-                              ],
+                      _pageHeader(context, size, "Editar Cita"),
+                      Material(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Container(
+                              alignment: Alignment.center,
+                              width: 359.0,
+                              height: 599.0,
+                              decoration: BoxDecoration(
+                                color: kBlanco,
+                                boxShadow: [
+                                  BoxShadow(
+                                      offset: Offset(0, 0),
+                                      blurRadius: 5.0,
+                                      color: Colors.grey.withOpacity(0.5)),
+                                ],
+                              ),
+                              child: Column(
+                                children: <Widget>[
+                                  _headerDate(context, cita),
+                                  SizedBox(
+                                    height: 10.0,
+                                  ),
+                                  secctionEdit('Hora:', hour, cita),
+                                  SizedBox(height: 2),
+                                  secctionEdit('Fecha:', date, cita),
+                                  SizedBox(height: 2),
+                                  secctionEdit('Costo', priceDate, cita),
+                                  SizedBox(height: 2),
+                                  secctionEdit('Detalles de Pago:',
+                                      paymentDetails, cita),
+                                  SizedBox(height: 2),
+                                  secctionEdit('Lugar:', place, cita),
+                                  SizedBox(height: 2),
+                                  secctionEdit(
+                                      'Especialidad:', specialty, cita),
+                                  SizedBox(height: 2),
+                                  secctionEdit('Tipo:', type, cita),
+                                  SizedBox(height: 2),
+                                  secctionEdit('Contacto:', contact, cita),
+                                  SizedBox(height: 2),
+                                  _dateState(context, cita),
+                                  _buttons(context, cita),
+                                ],
+                              ),
                             ),
-                            child: Column(
-                              children: <Widget>[
-                                _headerDate(context, cita),
-                                secction(title: 'Hora:', text: hour),
-                                secction(title: 'Fecha:', text: date),
-                                secction(title: 'Costo', text: priceDate),
-                                secction(
-                                    title: 'Detalles de pago:',
-                                    text: paymentDetails),
-                                secction(title: 'Lugar:', text: place),
-                                secction(
-                                    title: 'Especialidad:', text: specialty),
-                                secction(title: 'Tipo:', text: type),
-                                secction(title: 'Contacto:', text: contact),
-                                _dateState(context, cita),
-                                _buttons(context, cita),
-                              ],
-                            ),
-                          ),
-                        ],
+                          ],
+                        ),
                       ),
                     ],
                   ),
@@ -102,7 +113,6 @@ class DetalleCitaPro extends StatelessWidget {
 Widget _dateState(BuildContext context, Cita cita) {
   final bool state = cita.estado;
   return Container(
-    padding: EdgeInsets.only(top: 10.0),
     width: 270.0,
     height: 46,
     child: Stack(
@@ -114,7 +124,7 @@ Widget _dateState(BuildContext context, Cita cita) {
           style: TextStyle(
             fontSize: 14.0,
             color: kLetras,
-            fontFamily: 'PoppinsRegular',
+            fontFamily: 'PoppinSemiBold',
             decoration: TextDecoration.none,
           ),
         ),
@@ -146,6 +156,49 @@ Widget _stateIcon(bool state) {
   }
 }
 
+Widget secctionEdit(String title, String text, Cita cita) {
+  return Container(
+    margin: EdgeInsets.only(bottom: 2.0),
+    width: 270.0,
+    height: 46,
+    child: Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: <Widget>[
+        Container(
+          width: 270.0,
+          child: Text(
+            title,
+            textAlign: TextAlign.start,
+            style: TextStyle(
+              fontSize: 14.0,
+              color: Colors.black,
+              fontFamily: 'PoppinSemiBold',
+              decoration: TextDecoration.none,
+            ),
+          ),
+        ),
+        SizedBox(
+          height: 7.0,
+        ),
+        Container(
+          width: 270.0,
+          height: 14.0,
+          child: TextField(
+            textAlign: TextAlign.center,
+            style: TextStyle(
+                fontSize: 14.0, color: kLetras, fontFamily: 'PoppinsRegular'),
+            textAlignVertical: TextAlignVertical.center,
+            controller: TextEditingController()..text = text,
+            maxLines: 1,
+            enableInteractiveSelection: true,
+            autofocus: false,
+          ),
+        ),
+      ],
+    ),
+  ); //Boton como los de ejercicios queiro respirar, mindfulness, quiero meditar
+}
+
 Widget _headerDate(BuildContext context, Cita cita) {
   String pacientName = cita.paciente.nombreCompleto();
   return Container(
@@ -156,18 +209,6 @@ Widget _headerDate(BuildContext context, Cita cita) {
           margin: EdgeInsets.only(top: 10.0, bottom: 10.0),
           alignment: Alignment.center,
           child: Icon(Icons.location_on),
-        ),
-        GestureDetector(
-          onTap: () {
-            Navigator.pushNamed(context, 'editarCitaProfesional',
-                arguments: cita);
-          },
-          child: Container(
-              alignment: Alignment.centerRight,
-              margin: EdgeInsets.only(
-                top: 15.0,
-              ),
-              child: Icon(Icons.edit, size: 47)),
         ),
         Container(
           alignment: Alignment.center,
@@ -194,7 +235,7 @@ Widget _buttons(BuildContext context, Cita cita) {
   if (state == false) {
     return Container(
       width: 359.0,
-      padding: EdgeInsets.only(top: 50),
+      padding: EdgeInsets.only(top: 30.0),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: <Widget>[
@@ -232,7 +273,7 @@ Widget _buttons(BuildContext context, Cita cita) {
   } else {
     return Container(
       width: 359.0,
-      padding: EdgeInsets.only(top: 50),
+      padding: EdgeInsets.only(top: 30.0),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: <Widget>[
@@ -321,16 +362,19 @@ Widget _pageHeader(BuildContext context, Size size, String titulo) {
               child: Icon(
                 Icons.arrow_back_ios,
                 size: 30,
-                color: kNegro,
+                color: kLetras,
               )),
         ),
-        Container(
+        Center(
           child: Container(
             child: Text(
               titulo,
               textAlign: TextAlign.center,
               style: GoogleFonts.poppins(
-                  fontSize: 22, color: kNegro, decoration: TextDecoration.none),
+                  fontSize: 22,
+                  color: kNegro,
+                  decoration: TextDecoration.none,
+                  fontWeight: FontWeight.w400),
             ),
           ),
         ),
