b6085850d043e59b87a87a9fc4c64f6940e849e4
Santiago Caro Duque <santiagocaroduque@gmail.com>
2021-05-20 16:44:12 -0500

    services Done

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -7,15 +7,23 @@ abstract class AuthBase {
   Future<void> logOut();
 }
 
+/// Clase con metodos generales para el manejo de sesiones en Firebase
+///
+/// Principalmente usa metodos de [FirebaseAuth]
 class AuthService implements AuthBase {
   final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
 
+  /// Metodo que obtiene el usuario que esta actualmente en sesión
   @override
   Future<User> getCurrentUser() async {
     User user = _firebaseAuth.currentUser;
     return user;
   }
 
+  /// Metodo para iniciar sesión,
+  ///
+  /// Retorna el usuario una vez haya podido iniciar sesion
+  /// En caso de error retorna `null`
   @override
   Future<User> logIn(String email, String password) async {
     UserCredential user;
@@ -29,11 +37,13 @@ class AuthService implements AuthBase {
     return user.user;
   }
 
+  /// Metodo para salir de sesión
   @override
   Future<void> logOut() async {
     _firebaseAuth.signOut();
   }
 
+  /// Método para crear una cuenta con un correo y una contraseña
   @override
   Future<String> signUp(
       String email, String password, String displayName) async {
